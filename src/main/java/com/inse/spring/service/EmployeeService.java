0fa490b88461e13f62f3b449eab7f180511f896f
g_samudh <g_samudh@MOLDOVA.ENCS.concordia.ca>
2017-01-18 22:23:01 -0500

    Initial commit

---
+++ src/main/java/com/inse/spring/service/EmployeeService.java
@@ -0,0 +1,18 @@
+package com.inse.spring.service;
+
+import java.util.List;
+
+import com.inse.spring.model.Employee;
+
+public interface EmployeeService {
+
+	void saveEmployee(Employee employee);
+
+	List<Employee> findAllEmployees();
+
+	void deleteEmployeeBySsn(String ssn);
+
+	Employee findBySsn(String ssn);
+
+	void updateEmployee(Employee employee);
+}
